37cbf830e3a067bfcf0aa8a6ff136407decacf6b
Henry Oke <hoke@LMG-00053.local>
2022-02-05 10:53:10 -0500

    Feat: React-router for Client Routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter,
+    Routes,
+    Route
+  } from "react-router-dom";
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={
+        <main style={{ justifyContent: "center", display: "flex", marginTop: "8rem"}}>
+          <h1>Welcome To The Homepage</h1>
+        </main>
+      } />
+      <Route path="/resume" element={<App />} />
+      <Route path="*"  element={
+        <main style={{ padding: "1rem" }}>
+          <p>There's nothing here!</p>
+        </main>
+      } />
+    </Routes>
+  </BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
